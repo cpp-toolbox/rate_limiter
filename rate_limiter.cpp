9851d08dc5740d769c6beb7042e446180dcb3a79
ccn <ccn@cuppajoeman.com>
2025-07-07 17:37:34 -0400

    feat: add optional earliness leniency

--- rate_limiter.cpp
+++ rate_limiter.cpp
@@ -1,18 +1,30 @@
 #include "rate_limiter.hpp"
 
-RateLimiter::RateLimiter(double rate_limit_hz)
-    : rate_limit_duration(1.0 / rate_limit_hz),
-      last_run_time(std::chrono::steady_clock::now() -
-                    std::chrono::duration_cast<std::chrono::steady_clock::duration>(rate_limit_duration)) {}
+RateLimiter::RateLimiter(double rate_limit_hz, double earliness_leniency_seconds)
+    : rate_limit_duration(1.0 / rate_limit_hz), earliness_leniency_duration(earliness_leniency_seconds),
+      last_cycle_time(std::chrono::steady_clock::now() -
+                      std::chrono::duration_cast<std::chrono::steady_clock::duration>(rate_limit_duration)) {}
 
 double RateLimiter::get_last_processed_time() const { return last_processed_time; }
 
 bool RateLimiter::attempt_to_run() {
     auto now = std::chrono::steady_clock::now();
-    if (now - last_run_time >= rate_limit_duration) {
-        last_processed_time = std::chrono::duration<double>(now - last_run_time).count(); // store time in seconds
-        last_run_time = now;
-        return true;
+    auto elapsed_time_since_last_run = now - last_cycle_time;
+
+    auto shortest_required_duration = rate_limit_duration - earliness_leniency_duration;
+
+    if (elapsed_time_since_last_run >= shortest_required_duration) { // A
+        if (not ran_during_cycle) {
+            ran_during_cycle = true;
+            return true;
+        }
+    }
+
+    if (elapsed_time_since_last_run >= rate_limit_duration) { // B
+        // start of a new cycle
+        last_cycle_time = now;
+        ran_during_cycle = false;
+        // we don't need to return true here because  B => A
     }
     return false;
 }
--- rate_limiter.hpp
+++ rate_limiter.hpp
@@ -3,14 +3,18 @@
 
 #include <chrono>
 
+#include "sbpt_generated_includes.hpp"
+
 class RateLimiter {
   public:
-    explicit RateLimiter(double rate_limit_hz);
+    explicit RateLimiter(double rate_limit_hz, double earliness_leniency_seconds = 0.0);
+
+    ConsoleLogger logger{"rate_limiter"};
 
     bool attempt_to_run();
     bool attempt_to_run(double delta_time);
-
     void change_rate_limit(double rate_limit_hz);
+    void change_earliness_leniency(double earliness_leniency_seconds);
 
     double get_last_processed_time() const;
 
@@ -18,13 +22,14 @@ class RateLimiter {
     double last_processed_time = 0.0;
 
     std::chrono::duration<double> rate_limit_duration;
+    std::chrono::duration<double> earliness_leniency_duration;
     // AUTOMATIC TIME PROCESSING START
-    std::chrono::steady_clock::time_point last_run_time;
+    std::chrono::steady_clock::time_point last_cycle_time;
     // AUTOMATIC TIME PROCESSING END
-
     // MANUAL TIME PROCESSING START
     double manual_time_processed_since_last_tick = 0.0;
     // MANUAL TIME PROCESSING END
-};
 
+    bool ran_during_cycle = false;
+};
 #endif // RATELIMITER_HPP
